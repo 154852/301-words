d85656538e2285d95415a08919cd029c236132bc
154852 <louisemileploix@icloud.com>
2018-12-14 16:51:56 +0000

    Added main site

--- js/utils.js
+++ js/utils.js
@@ -1,5 +1,5 @@
 function openPage(page, target) {
-    document.body.classList.add('page-exit');
+    document.body.classList.add('page-exit'); document.body.classList.remove('page-open');
 
     if (page) setTimeout(() => window.open(page, target), 500);
 }
@@ -12,3 +12,12 @@ function getData(cb) {
     xhr.open('GET', 'app/data.json');
     xhr.send();
 }
+
+function escapeHTML(unsafe) {
+    return unsafe
+         .replace(/&/g, "&amp;")
+         .replace(/</g, "&lt;")
+         .replace(/>/g, "&gt;")
+         .replace(/"/g, "&quot;")
+         .replace(/'/g, "&#039;");
+ }
